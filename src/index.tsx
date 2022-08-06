0b2d235c6f33a07e0574af0e93ffb2f74e7d0772
SunlightLuck <sunlight.luck@gmail.com>
2022-08-06 16:08:34 -0400

    redux added

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider as StoreProvider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Provider from "./Provider";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <StoreProvider store={store}>
+      <Provider>
+        <App />
+      </Provider>
+    </StoreProvider>
   </React.StrictMode>
 );
 
