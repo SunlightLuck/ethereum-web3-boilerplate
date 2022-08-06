c1ebabe0881ab0c45c55a54988b038bcfe66d532
SunlightLuck <sunlight.luck@gmail.com>
2022-08-06 18:25:10 -0400

    complete

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,8 @@
 import { useWeb3React } from "@web3-react/core";
 import { connectorsByName } from "../utils/web3React";
 
+declare let window: any;
+
 const useAuth = () => {
   const { activate, deactivate } = useWeb3React();
 
@@ -15,8 +17,20 @@ const useAuth = () => {
       alert("The connector config is wriong");
     }
   };
+  const switchChain = async (chainId: string) => {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{chainId}]
+    })
+  }
+  const addChain = async (params: any) => {
+    await window.ethereum.request({
+      method: 'wallet_addEthereumChain',
+      params
+    })
+  }
 
-  return { login, logout: deactivate };
+  return { login, logout: deactivate, switchChain, addChain };
 };
 
 export default useAuth;
